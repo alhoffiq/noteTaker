4aa570acfaa5c18be490fc1ad9fe9340bb17ca2d
alhoff <andrew.hoff14@yahoo.com>
2020-10-28 18:22:46 -0700

    added start script

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -50,5 +50,5 @@ app.delete("/api/notes/:id", function (req, res) {
 
 });
 app.listen(PORT, function () {
-    console.log("App listening on PORT");
+    console.log(`App listening on PORT:${PORT}`);
 });
